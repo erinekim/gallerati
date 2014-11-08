684b760b356fbbb2de1e1749556fee0ed0dd4f62
Erin Kim <erin.e.kim@gmail.com>
2014-11-08 19:15:50 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gallerati::Application.config.secret_key_base = '541d838a6c26affa061c304d52a08eb2c9df885f99a8666ac5f85e297f4433b9968a9ed014eb64f3b26f2dcd2071050ac669365eaa9a3ee19286f1edad4d9423'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gallerati::Application.config.session_store :cookie_store, key: '_gallerati_session'
